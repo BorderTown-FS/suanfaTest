dbfbc986190325ca1ec0166c05d8436dd6dd325f
BorderTown-FS <13006306506@163.com>
2021-01-14 17:04:51 +0800

    新增

---
+++ src/algorithmProblem/FindTheLongestStr.java
@@ -0,0 +1,12 @@
+package algorithmProblem;
+
+public class FindTheLongestStr {
+    public static void mainFindTheLongestStr() {
+        String[] rom = {};
+        System.out.println("结果为："+longestCommonPrefix(rom));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        return "";
+    }
+}
--- src/algorithmProblem/RomanToInt.java
+++ src/algorithmProblem/RomanToInt.java
@@ -1,35 +1,73 @@
 package algorithmProblem;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 13. 罗马数字转数字
  */
 public class RomanToInt {
 
     public static void mainRomanToInt() {
-        String rom = "V";
-        System.out.println("结果为："+romanToInt(rom));
+        String rom = "III";
+        System.out.println("结果为："+learnIntToRoman(rom));
     }
 
     /**
-     * 暴力破解
+     * 借鉴14的算法不行
      */
-    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-    String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    static int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    static String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
 
-    public int romanToInt(String rom) {
-        int sum = 0;
-        char[] romChar = rom.toCharArray();
+    public static int intToRomanOwn(String str) {
+        int result = 0;
+        int j = 0;
         // Loop through each symbol, stopping if num becomes 0.
-        for (int i = 0; i <romChar.length; i++) {
+        for (int i = 0; i < str.toCharArray().length; i++) {
             // Repeat while the current symbol still fits into num.
-            for(int j = 0;j<symbols.length;j++){
-                String c = romChar[i]+"";
-                if(c.equals(symbols[j])){
-                    sum = sum+values[j];
+            for(;j<symbols.length;){
+                if(str.substring(i,symbols[j].length()+i).equals("I")){
+                    result = result + 1;
+                    break;
+                } else if(str.substring(i,symbols[j].length()+i).equals(symbols[j])){
+                    result = result + values[j];
+                    j++;
+                    break;
+                }else{
+                    j++;
                 }
             }
         }
-        return sum;
+        return result;
+    }
+
+    public static int learnIntToRoman(String str) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("I", 1);
+        map.put("IV", 4);
+        map.put("V", 5);
+        map.put("IX", 9);
+        map.put("X", 10);
+        map.put("XL", 40);
+        map.put("L", 50);
+        map.put("XC", 90);
+        map.put("C", 100);
+        map.put("CD", 400);
+        map.put("D", 500);
+        map.put("CM", 900);
+        map.put("M", 1000);
+
+        int result = 0;
+        for (int i = 0; i < str.toCharArray().length;) {
+            if(i+1<str.length() && map.containsKey(str.substring(i,i+2))){
+                result += map.get(str.substring(i,i+2));
+                i+=2;
+            }else{
+                result += map.get(str.substring(i,i+1));
+                i++;
+            }
+        }
+        return result;
     }
 }
 
